ebdd8b1fceec234e066f7c0d397ae58f4ed9c699
Yidi-Huang <yidi.huang.elodie@gmail.com>
2025-01-24 12:10:02 +0100

    test

---
+++ Formation_C_sharp/Percolation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Percolation
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("hi");
+            Console.ReadKey();
+        }
+    }
+}
