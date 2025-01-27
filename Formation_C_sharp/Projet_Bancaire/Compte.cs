100e794ea3b5f89e62d1afd32449c6d05062675b
Yidi-Huang <yidi.huang.elodie@gmail.com>
2025-01-27 17:09:26 +0100

    update compte

--- Formation_C_sharp/Projet_Bancaire/Compte.cs
+++ Formation_C_sharp/Projet_Bancaire/Compte.cs
@@ -66,5 +66,62 @@ namespace Projet_Bancaire
 
             return comptes;
         }
+
+        public string AjoutSolde(decimal somme)
+        {
+            string status;
+            if (somme<0)
+            {
+                status = "KO";
+            }
+            else
+            {
+                solde += somme;
+                status = "OK";
+            }
+            return status;
+        }
+
+        public bool evaluateMax(List<decimal> ls, decimal somme)
+        {
+            decimal sum = 0m;
+            bool estInfer = false;
+            if (ls.Count >9)
+            {
+                ls.RemoveAt(0);
+            }
+            foreach (decimal s in ls)
+            {
+                sum += s;
+            }
+            sum += somme;
+
+            if (sum < 1000)
+            {
+                estInfer = true;
+            }
+            else
+            {
+                estInfer = false;
+            }
+            return estInfer;
+        }
+
+        public string RetraitSolde(decimal somme)
+        {
+            string status;
+            if (solde <= somme)                 // 10 derniers virems > 1000
+            {
+                status = "KO";
+            }
+            else
+            {
+                solde -= somme;
+                status = "OK";
+                his_soldes.Add(somme);
+            }
+            return status;
+        }
+
     }
 }
